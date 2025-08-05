f2f1f88092d3c95ba532077d9df1b521524db2a5
Kataleko <katalekoweb@gmail.com>
2025-08-05 21:45:02 +0100

    feat: test home page contains non empty table

--- tests/Feature/ProductsTest.php
+++ tests/Feature/ProductsTest.php
@@ -3,14 +3,29 @@
 namespace Tests\Feature;
 
 // use Illuminate\Foundation\Testing\RefreshDatabase;
+
+use App\Models\Product;
 use Tests\TestCase;
 
 class ProductsTest extends TestCase
 {
-    public function test_example(): void
+    public function test_home_page_contains_empty_table(): void
     {
         $response = $this->get('/products');
         $response->assertSee(__('No records found'));
         $response->assertStatus(200);
     }
+
+    public function test_home_page_contains_non_empty_table(): void
+    {
+        Product::query()->create([
+            "name" => "Product 1",
+            "price" => 100
+        ]);
+
+        $response = $this->get('/products');
+        $response->assertDontSee(__('No records found'));
+        $response->assertStatus(200);
+    }
+
 }
